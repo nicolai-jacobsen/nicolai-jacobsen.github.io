bafaabc2dde7980e79770c79c80a15b14f3ad867
nicolai-jacobsen <jacobsen.nicolai02@gmail.com>
2024-01-25 10:59:18 -0500

    updated website

---
+++ main.js
@@ -0,0 +1,30 @@
+const notifierButton = document.getElementById("notifierButton")
+
+notifierButton.addEventListener('click', function(){
+    document.getElementById("popup").style.display = 'block';
+})
+
+const closeButton = document.getElementById('close-popup-btn')
+
+closeButton.addEventListener('click', function(){
+    document.getElementById("popup").style.display = 'none';
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    var images = [
+        'images/photo1.jpg',
+        'images/photo2.jpg',
+    ];
+
+    var currentIndex = 0;
+    var imgElement = document.getElementById('rotating-image');
+
+    setInterval(function() {
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+
+        imgElement.src = images[currentIndex];
+    }, 5000);
+});
